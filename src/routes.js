63fcd958e7d32ce917a22cb328d46906bbfa1a19
Matheus Rocha <matheus_rocha98@outlook.com>
2020-05-07 19:22:32 -0300

    refactor: creating routes for the new pages

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-// import { Container } from './styles';
+import Auth from './pages/Auth';
+import Dummy from './pages/Dummy';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/auth" component={Auth} />
+      <Route path="/dummy" component={Dummy} />
     </Switch>
   );
 }
