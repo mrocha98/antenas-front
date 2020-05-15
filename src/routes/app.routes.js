7232bf8df08a2b75951857436c36ed65c3a8586f
Matheus Rocha <matheus_rocha98@outlook.com>
2020-05-15 00:22:38 -0300

    refactor: improving routes paths to support a default route
    In auth.routes login is default and 404 error redirects to login.
    In app.routes dashboard is default, root redirects to dashboard, and 404 eror redirects to NotFound page.

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from '../components/Menu';
 import Home from '../pages/Home';
 import Dummy from '../pages/Dummy';
+import NotFound from '../pages/NotFound';
 
 export default function AppRoutes() {
   return (
     <>
       <Menu />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/dashboard" component={Home} />
         <Route path="/dummy" component={Dummy} />
+        <Route path="/" exact render={() => <Redirect to="/dashboard" />} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </>
   );
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 
 export default function AuthRoutes() {
   return (
     <Switch>
-      <Route path="/" exact component={Login} />
       <Route path="/register" component={Register} />
-      <Route path="*" component={Login} />
+      <Route path="/login" component={Login} />
+      <Route path="*" render={() => <Redirect to="/login" />} />
     </Switch>
   );
 }
