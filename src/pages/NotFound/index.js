1b7a90ca17e267f38c733acd64cccec8c0b9f176
Matheus Rocha <matheus_rocha98@outlook.com>
2020-05-15 00:21:05 -0300

    feat: creating NotFound page

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { File } from 'react-kawaii';
+import { useHistory } from 'react-router-dom';
+import './styles.scss';
+
+export default function NotFound() {
+  const history = useHistory();
+  const [fileMood, setFileMood] = useState('ko');
+
+  const handleMouseEnter = () => setFileMood('happy');
+  const handleMouseLeave = () => setFileMood('ko');
+
+  return (
+    <article className="page page--not_found">
+      <Typography variant="h1">Erro 404</Typography>
+      <div className="container">
+        <File mood={fileMood} color="#dfdfdf" />
+      </div>
+      <Typography variant="h4">Página não encontrada</Typography>
+      <div className="container">
+        <Button
+          color="secondary"
+          onClick={() => history.push('/dashboard')}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          Voltar à civilização
+        </Button>
+      </div>
+    </article>
+  );
+}
