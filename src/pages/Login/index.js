4414b3fca541dd902a8b263e83d74df9ccc1356b
Matheus Rocha <matheus_rocha98@outlook.com>
2020-05-18 23:02:58 -0300

    feat: implementing yup validation and displaying errors

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { Typography, TextField, Button, Divider } from '@material-ui/core';
+import React, { useState } from 'react';
+import * as yup from 'yup';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import PageContainer from '../../components/PageContainer';
@@ -8,14 +13,27 @@ import AuthForm from '../../components/AuthForm';
 import Field from '../../components/Field';
 import { useAuth } from '../../contexts/auth';
 
+const schema = yup.object().shape({
+  email: yup.string().trim().email().required('E-mail é obrigatório'),
+  password: yup.string().required('Senha é obrigatória'),
+});
+
 function Login() {
+  const [signInError, setSignInError] = useState(false);
   const history = useHistory();
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit, register, errors } = useForm({
+    validationSchema: schema,
+  });
   const { signIn } = useAuth();
 
-  const onSubmit = (data, event) => {
+  const onSubmit = async (data, event) => {
     event.preventDefault();
-    signIn(data.email, data.password);
+
+    try {
+      await signIn(data.email, data.password);
+    } catch {
+      setSignInError(true);
+    }
   };
 
   return (
@@ -24,11 +42,17 @@ function Login() {
         Antenas
       </Typography>
       <AuthCard>
-        <AuthForm
-          title="Autentique-se"
-          onSubmit={handleSubmit(onSubmit)}
-          noValidate
-        >
+        <AuthForm title="Autentique-se" onSubmit={handleSubmit(onSubmit)}>
+          {signInError && (
+            <Field>
+              <Alert severity="error">E-mail e/ou senha inválidos.</Alert>
+            </Field>
+          )}
+          {errors.email && (
+            <Field>
+              <Alert severity="warning">{errors.email.message}</Alert>
+            </Field>
+          )}
           <Field>
             <TextField
               name="email"
@@ -40,6 +64,11 @@ function Login() {
               fullWidth
             />
           </Field>
+          {errors.password && (
+            <Field>
+              <Alert severity="warning">{errors.password.message}</Alert>
+            </Field>
+          )}
           <Field>
             <TextField
               name="password"
