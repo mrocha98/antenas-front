e42a95b32995d1ba66a78b80dfb31a08a75d4c23
Matheus Rocha <matheus_rocha98@outlook.com>
2020-05-26 18:39:33 -0300

    feat: a lot of stuff
    sorry for not providing a good description

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'react/jsx-props-no-spreading': 'off',
     'import/prefer-default-export': 'off',
     'react/prop-types': 'off',
     'react-hooks/rules-of-hooks': 'error',
--- src/components/AuthCard/index.js
+++ src/components/AuthCard/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Card as MCard, CardContent } from '@material-ui/core';
-import './styles.scss';
 
 function AuthCard({ children }) {
   return (
-    <MCard className="auth-card">
+    <MCard>
       <CardContent>{children}</CardContent>
     </MCard>
   );
