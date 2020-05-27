eafdab6e28616bcb11fc90be9916e6f28b536869
Matheus Rocha <matheus_rocha98@outlook.com>
2020-05-26 22:05:34 -0300

    refactor: fixed wrong stuff in auth service and create btn to hide passw
    i promise to write beautiful commits in my next contribution

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,11 @@
-/* eslint-disable no-param-reassign */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
+import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -27,171 +29,203 @@ function Register() {
     mode: 'onBlur',
   });
   const { signIn } = useAuth();
-  const [registerError, setRegisterError] = useState(false);
-  const [selectedType, setSelectedType] = useState(UserTypes.ALUNO);
-  const typeWatcher = watch('type');
+  const [registerError, setRegisterError] = useState({
+    display: false,
+    message: '',
+  });
+  const [showPassword, setShowPassword] = useState(true);
+  const selectedType = watch('type');
+
+  const handleShowPassword = () => setShowPassword(!showPassword);
+  const choosePasswordIcon = () => (showPassword ? <FaEye /> : <FaEyeSlash />);
 
   const onSubmit = async (data, event) => {
     event.preventDefault();
-    data.isActive = true;
+    const { name, email, password, type, position, company, cnpj } = data;
     try {
-      const created = await createUser(data);
-      if (created) await signIn(data.email, data.password);
+      const created = await createUser({
+        name,
+        email,
+        password,
+        type,
+        position,
+        company,
+        cnpj,
+      });
+      if (created) await signIn({ email, password });
     } catch (err) {
-      console.log(err);
-      setRegisterError(true);
+      setRegisterError({ display: true, message: err.response.data.error });
     }
   };
 
-  useEffect(() => {
-    if (typeWatcher !== selectedType) setSelectedType(typeWatcher);
-  }, [typeWatcher, selectedType]);
-
   return (
     <Container maxWidth="sm">
-      <Typography variant="h2" component="h1" align="center">
-        Antenas
-      </Typography>
-      <AuthCard>
-        <AuthForm title="Crie uma nova conta" onSubmit={handleSubmit(onSubmit)}>
-          {registerError && (
-            <Field>
-              <Alert severity="warning">Esse e-mail já foi cadastrado</Alert>
-            </Field>
-          )}
-          <Field>
-            <TextField
-              label="Nome"
-              name="name"
-              variant="outlined"
-              inputRef={register}
-              fullWidth
-              error={!!errors.name}
-              helperText={errors.name?.message}
-            />
-          </Field>
-          <Field>
-            <TextField
-              label="E-mail"
-              name="email"
-              type="email"
-              variant="outlined"
-              inputRef={register}
-              fullWidth
-              error={!!errors.email}
-              helperText={errors.email?.message}
-            />
-          </Field>
-          <Field>
-            <TextField
-              label="Senha"
-              name="password"
-              type="password"
-              variant="outlined"
-              inputRef={register}
-              fullWidth
-              error={!!errors.password}
-              helperText={errors.password?.message}
-            />
-          </Field>
-          <Field>
-            <InputLabel
-              style={{ marginBottom: '0.5rem' }}
-              error={!!errors.type}
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h2" component="h1" align="center">
+            Antenas
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <AuthCard>
+            <AuthForm
+              title="Crie uma nova conta"
+              onSubmit={handleSubmit(onSubmit)}
             >
-              Categoria
-            </InputLabel>
-            <Controller
-              name="type"
-              defaultValue=""
-              control={control}
-              as={
-                <Select variant="outlined" fullWidth error={!!errors.type}>
-                  <MenuItem value={UserTypes.EMPRESARIO}>Empresário</MenuItem>
-                  <MenuItem value={UserTypes.CADI}>CADI</MenuItem>
-                  <MenuItem value={UserTypes.ALUNO}>Aluno</MenuItem>
-                  <MenuItem value={UserTypes.PROFESSOR}>Professor</MenuItem>
-                </Select>
-              }
-            />
-            <FormHelperText error>{errors.type?.message}</FormHelperText>
-          </Field>
-          {selectedType === UserTypes.CADI && (
-            <Field>
-              <Controller
-                name="position"
-                control={control}
-                defaultValue=""
-                as={
-                  <TextField
-                    label="Cargo"
-                    variant="outlined"
-                    fullWidth
-                    error={!!errors.position}
-                    helperText={errors.position?.message}
+              {registerError.display && (
+                <Field>
+                  <Alert severity="warning">{registerError.message}</Alert>
+                </Field>
+              )}
+              <Field>
+                <TextField
+                  label="Nome"
+                  name="name"
+                  variant="outlined"
+                  inputRef={register}
+                  fullWidth
+                  autoFocus
+                  error={!!errors.name}
+                  helperText={errors.name?.message}
+                />
+              </Field>
+              <Field>
+                <TextField
+                  label="E-mail"
+                  name="email"
+                  type="email"
+                  variant="outlined"
+                  inputRef={register}
+                  fullWidth
+                  error={!!errors.email}
+                  helperText={errors.email?.message}
+                />
+              </Field>
+              <Field>
+                <TextField
+                  label="Senha"
+                  name="password"
+                  type={showPassword ? 'password' : 'text'}
+                  variant="outlined"
+                  inputRef={register}
+                  fullWidth
+                  error={!!errors.password}
+                  helperText={errors.password?.message}
+                  InputProps={{
+                    endAdornment: (
+                      <IconButton
+                        color="default"
+                        size="small"
+                        onClick={handleShowPassword}
+                      >
+                        {choosePasswordIcon()}
+                      </IconButton>
+                    ),
+                  }}
+                />
+              </Field>
+              <Field>
+                <InputLabel
+                  style={{ marginBottom: '0.5rem' }}
+                  error={!!errors.type}
+                >
+                  Categoria
+                </InputLabel>
+                <Controller
+                  name="type"
+                  defaultValue=""
+                  control={control}
+                  as={
+                    <Select variant="outlined" fullWidth error={!!errors.type}>
+                      <MenuItem value={UserTypes.EMPRESARIO}>
+                        Empresário
+                      </MenuItem>
+                      <MenuItem value={UserTypes.CADI}>CADI</MenuItem>
+                      <MenuItem value={UserTypes.ALUNO}>Aluno</MenuItem>
+                      <MenuItem value={UserTypes.PROFESSOR}>Professor</MenuItem>
+                    </Select>
+                  }
+                />
+                <FormHelperText error>{errors.type?.message}</FormHelperText>
+              </Field>
+              {selectedType === UserTypes.CADI && (
+                <Field>
+                  <Controller
+                    name="position"
+                    control={control}
+                    defaultValue=""
+                    as={
+                      <TextField
+                        label="Cargo"
+                        variant="outlined"
+                        fullWidth
+                        error={!!errors.position}
+                        helperText={errors.position?.message}
+                      />
+                    }
                   />
-                }
-              />
-            </Field>
-          )}
-          {selectedType === UserTypes.EMPRESARIO && (
-            <>
-              <Field>
-                <Controller
-                  name="company"
-                  control={control}
-                  defaultValue=""
-                  as={
-                    <TextField
-                      label="Nome da Empresa"
-                      variant="outlined"
-                      fullWidth
-                      error={!!errors.company}
-                      helperText={errors.company?.message}
+                </Field>
+              )}
+              {selectedType === UserTypes.EMPRESARIO && (
+                <>
+                  <Field>
+                    <Controller
+                      name="company"
+                      control={control}
+                      defaultValue=""
+                      as={
+                        <TextField
+                          label="Nome da Empresa"
+                          variant="outlined"
+                          fullWidth
+                          error={!!errors.company}
+                          helperText={errors.company?.message}
+                        />
+                      }
                     />
-                  }
-                />
-              </Field>
-              <Field>
-                <Controller
-                  name="cnpj"
-                  control={control}
-                  defaultValue=""
-                  as={
-                    <TextField
-                      label="CNPJ"
-                      variant="outlined"
-                      fullWidth
-                      error={!!errors.cnpj}
-                      helperText={errors.cnpj?.message}
+                  </Field>
+                  <Field>
+                    <Controller
+                      name="cnpj"
+                      control={control}
+                      defaultValue=""
+                      as={
+                        <TextField
+                          label="CNPJ"
+                          variant="outlined"
+                          fullWidth
+                          error={!!errors.cnpj}
+                          helperText={errors.cnpj?.message}
+                        />
+                      }
                     />
-                  }
-                />
+                  </Field>
+                </>
+              )}
+              <Field applyHugeDistance>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  size="large"
+                  color="primary"
+                  fullWidth
+                >
+                  Confirmar
+                </Button>
               </Field>
-            </>
-          )}
-          <Field applyHugeDistance>
-            <Button
-              variant="contained"
-              type="submit"
-              size="large"
-              color="primary"
-              fullWidth
-            >
-              Confirmar
-            </Button>
-          </Field>
-          <Divider variant="middle" />
-          <Button
-            variant="text"
-            color="secondary"
-            style={{ marginTop: '1em' }}
-            onClick={() => history.push('/login')}
-          >
-            Já tenho uma conta
-          </Button>
-        </AuthForm>
-      </AuthCard>
+              <Divider variant="middle" />
+              <Button
+                variant="text"
+                color="primary"
+                style={{ marginTop: '1em' }}
+                onClick={() => history.push('/login')}
+              >
+                Já tenho uma conta
+              </Button>
+            </AuthForm>
+          </AuthCard>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
