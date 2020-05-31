036134ff2e9254d715ffc33ddbf3d7d07b44b905
Matheus Rocha <matheus_rocha98@outlook.com>
2020-05-30 22:01:37 -0300

    refactor: adding projects route in empresario.routes

--- src/routes/empresario.routes.js
+++ src/routes/empresario.routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from '../components/Menu';
 import Dashboard from '../layouts/empresario/dashboard';
+import Projects from '../layouts/empresario/projects';
 import NotFound from '../components/NotFound';
 
 export default function EmpresarioRoutes() {
@@ -12,6 +13,7 @@ export default function EmpresarioRoutes() {
       <Menu rootHash={rootHash} />
       <Switch>
         <Route path={baseUrl} exact component={Dashboard} />
+        <Route path={`${baseUrl}/projects`} component={Projects} />
         <Route path="/" exact render={() => <Redirect to={baseUrl} />} />
         <Route path="*" render={() => <NotFound />} />
       </Switch>
