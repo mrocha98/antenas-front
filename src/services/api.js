0361accf223f83a8bfd9e173d1a90ac328c94251
Matheus Rocha <matheus_rocha98@outlook.com>
2020-09-08 20:30:01 -0300

    feat: implement .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const { REACT_APP_API_URL } = process.env;
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: REACT_APP_API_URL,
 });
 
 export default api;
